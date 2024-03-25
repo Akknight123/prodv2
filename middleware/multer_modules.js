4478522fdbb4386f30ad42dbdf442cbd993b4c15
Akknight123 <amanbeniwal78@gmail.com>
2024-03-25 21:54:57 +0530

    vercel

--- middleware/multer_modules.js
+++ middleware/multer_modules.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'tmp/')
+        cb(null, '/tmp')
     },
     filename: function (req, file, cb) {
         const dateTime = Date.now();
