01d0b12282b292c06bcab9f5ef52224bd4ba3a95
Akknight123 <amanbeniwal78@gmail.com>
2024-03-26 14:34:55 +0530

    rollback to public/assets

--- middleware/multer_modules.js
+++ middleware/multer_modules.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, process.cwd())
+        cb(null, 'public/assets')
     },
     filename: function (req, file, cb) {
         const dateTime = Date.now();
